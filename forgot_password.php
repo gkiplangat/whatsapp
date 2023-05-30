e9ea1a56ee2e9971b5ef64357048cd346ee47347
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-05-30 16:57:13 +0300

    updated files

---
+++ forgot_password.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <style>
+        body,
+        html {
+            height: 100%;
+        }
+
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+        }
+
+        .card {
+            width: 350px;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title text-center">Forgot Password</h3>
+                <form>
+                    <div class="form-group">
+                        <label for="username">Email</label>
+                        <input type="text" class="form-control" id="username" placeholder="Enter your email">
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary btn-block">Login</button>
+                    <br>
+                    <p><a href="index.php">Back to login</a></p>
+                    <p>Do no have an Account <a href="#">Create Here</a></p>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
