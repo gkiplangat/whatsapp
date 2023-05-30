587acc4c0ae2566da4447893948281725c0d11c5
Gideon Kiplangat <gideonkiplangat4@gmail.com>
2023-05-30 16:06:22 +0300

    database connection implemented successfully

---
+++ dbconn.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost"; //server name
+$username = "root"; //database username
+$password = ""; //database password
+$dbname = "whatsapp"; //database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+// Close connection
+$conn->close();
